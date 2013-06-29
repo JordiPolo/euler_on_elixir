07f254d124690999d26e2bea05e3dad10a1042f8
Jordi Polo <mumismo@gmail.com>
2013-06-29 23:51:51 +0900

    write code the elixir way

--- lib/euler_on_elixir.ex
+++ lib/euler_on_elixir.ex
@@ -1,5 +1,4 @@
 defmodule EulerOnElixir do
-#defmodule Euler do
 
   def main(args) do
     definitions
@@ -22,14 +21,10 @@ defmodule EulerOnElixir do
   end
 
   def problems do
-    files = Path.wildcard "lib/*.ex"
-    filenames = Enum.map files, Path.basename(&1)
-    #      |> Path.wildcard
-  #  |> Enum.map fn path -> Path.basename(path) end
-   #   |> Enum.filter(fn filename -> valid_filename?(filename) end)
-    #  |> Enum.map fn valid_name -> String.replace(valid_name, ".ex", "") end
-      filenames = Enum.filter(filenames, valid_filename?(&1))
-      Enum.map filenames, String.replace(&1, ".ex", "")
+    Path.wildcard("lib/*.ex")
+     |> Enum.map(Path.basename(&1))
+     |> Enum.filter(valid_filename?(&1))
+     |> Enum.map( String.replace(&1, ".ex", ""))
   end
 
   defp valid_filename?(filename) do
@@ -39,9 +34,9 @@ defmodule EulerOnElixir do
 
   #TODO: why can not connect pipes?
   defp modules do
-    numbers = problems
-    |> Enum.map String.replace(&1, "0", "")
-    Enum.map numbers, fn x -> "Problem#{x}" end
+    problems
+     |> Enum.map(String.replace(&1, "0", ""))
+     |> Enum.map(fn x -> "Problem#{x}" end)
   end
 
   defp module_doc(module) do
